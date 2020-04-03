0d265cb439563d748821dd4968d1fd920ddf3b57
Dustin Myers <dustinblaine.myers@gmail.com>
2020-04-03 07:31:26 -0400

    Including 3 way join sql statement

--- sql/queries.sql
+++ sql/queries.sql
@@ -1,3 +1,5 @@
+-- Show all a users lights/presentations
+
 select test_lesson.name , users.name , lights.green_light
                 from test_lesson   inner join  lights on test_lesson.id = lights.lesson_id
                 inner join users on lights.users_id = users.id WHERE users.id = 1
