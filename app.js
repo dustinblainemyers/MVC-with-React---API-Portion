2775a8763d5d4582554a88a31fe31b38402c6043
Dustin Myers <dustinblaine.myers@gmail.com>
2020-04-28 15:23:54 -0400

    required and initialized dotenv extension

--- app.js
+++ app.js
@@ -1,47 +1,48 @@
-const express = require('express'),
-es6Renderer = require('express-es6-template-engine'),
-app = express();
+const express = require("express"),
+  es6Renderer = require("express-es6-template-engine"),
+  app = express();
 const http = require("http");
 const socketIo = require("socket.io");
 const axios = require("axios");
-const path = require('path');
+const path = require("path");
 cors = require("cors");
+const dotenv = require("dotenv");
+dotenv.config();
 
+const port = process.env.PORT;
+console.log(`Your port is ${port}`);
 
 const corsOptions = {
-    origin: "*",
-    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
-    preflightContinue: false,
-    optionsSuccessStatus: 204,
-    "Access-Control-Allow-Origin": "*",
-    "Access-Control-Allow-Headers":
-      "Origin, X-Requested-With, Content-Type, Accept"
-  };
+  origin: "*",
+  methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+  preflightContinue: false,
+  optionsSuccessStatus: 204,
+  "Access-Control-Allow-Origin": "*",
+  "Access-Control-Allow-Headers":
+    "Origin, X-Requested-With, Content-Type, Accept",
+};
 
 app.use(cors(corsOptions));
-app.use('/css',express.static(path.join(__dirname, 'public/css')));
+app.use("/css", express.static(path.join(__dirname, "public/css")));
 
 //middle ware
-app.engine('html',es6Renderer);
-app.set('views', 'views');
-app.set('view engine', 'html');
+app.engine("html", es6Renderer);
+app.set("views", "views");
+app.set("view engine", "html");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 const server = http.createServer(app);
 
-const io = socketIo(server); 
-
+const io = socketIo(server);
 
 const getApiAndEmit = async (socket, token) => {
   try {
     const res = await axios.get(
-      
       `http://localhost:3333/join-presentation/aggregate/countall/${token}`
-    ); 
-    socket.emit("FromAPI", res.data); 
-   
+    );
+    socket.emit("FromAPI", res.data);
   } catch (error) {
     console.error(`Error: ${error.code}`);
   }
@@ -49,9 +50,9 @@ const getApiAndEmit = async (socket, token) => {
 
 let interval;
 
-io.on("connection", socket => {
+io.on("connection", (socket) => {
   let token = socket.handshake.query.token;
-  console.log("New client connected",token);
+  console.log("New client connected", token);
   if (interval) {
     clearInterval(interval);
   }
@@ -64,21 +65,18 @@ io.on("connection", socket => {
 server.listen(4001, () => console.log(`Listening on port 4001`));
 
 app.listen(3333, () => {
-    console.log('Server running on port 3333');
-})
+  console.log("Server running on port 3333");
+});
 
-const rootController = require('./routes/index')
-const createPController = require('./routes/create-presentation')
-const joinPController = require(`./routes/join-presentation`)
-const userController = require(`./routes/users`)
-const miscController = require(`./routes/misc-endpoints`)
+const rootController = require("./routes/index");
+const createPController = require("./routes/create-presentation");
+const joinPController = require(`./routes/join-presentation`);
+const userController = require(`./routes/users`);
+const miscController = require(`./routes/misc-endpoints`);
 
+app.use("/", rootController);
 
-app.use('/', rootController);
-
-app.use('/create-presentation', createPController);
-app.use('/join-presentation', joinPController);
-app.use('/users', userController);
-app.use('/misc-endpoints', miscController);
-
-
+app.use("/create-presentation", createPController);
+app.use("/join-presentation", joinPController);
+app.use("/users", userController);
+app.use("/misc-endpoints", miscController);
